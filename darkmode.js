57b3417115516c78032454b7dff7361026a02f04
Bodnar_Gabor_iskolai <bistvngbor@mitiszkad.lan>
2023-10-02 09:24:36 +0200

    2.commit

---
+++ darkmode.js
@@ -0,0 +1,29 @@
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+
+  /*
+  function emailellenorzo() {
+      var emailInput = document.getElementById('email');
+      var email = emailInput.value;
+      
+      if (email.includes("/^\S+@\S+\.\S+$/")== true) {
+          document.getElementById('helyes').innerHTML="helyes";
+      } else {
+          document.getElementById('helytelen').innerHTML="helytelen";
+      }
+  }
+*/
+/*
+  function validateEmail() {
+    var re = /\S+@\S+\.\S+/;
+    re.test(email);
+    if (re.test(email) == true) {
+        document.getElementById('helyes').innerHTML="helyes";
+    } else {
+        document.getElementById('helytelen').innerHTML="helytelen";
+    }
+  }
+  */
